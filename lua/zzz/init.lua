7c28915813bc5bbaa7b4c863a25ade6ed781ef41
zcat <46110056+zoeyalex@users.noreply.github.com>
2024-12-28 12:27:25 +0100

    Set tabs to 4

--- lua/zzz/init.lua
+++ lua/zzz/init.lua
@@ -6,6 +6,7 @@ vim.opt.termguicolors = true
 vim.cmd.colorscheme 'modus'
 -- Set indentation to 4 spaces
 vim.opt.tabstop = 4       -- Number of spaces that a <Tab> counts for
+vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4    -- Number of spaces to use for each step of (auto)indent
 vim.opt.expandtab = true  -- Convert tabs to spaces
 vim.opt.smartindent = true -- Smart indentation
